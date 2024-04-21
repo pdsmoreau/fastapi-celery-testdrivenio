a6d1096f501ea0d4e5d87ba6aba8604a220f0898
Peu <pdsmoreau@pop-os.localdomain>
2024-04-21 10:21:15 -0300

    containerizing application

--- project/__init__.py
+++ project/__init__.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
-from project.celery_utils import create_celery
+
+from project.celery_utils import create_celery  # new
 
 
 def create_app() -> FastAPI:
     app = FastAPI()
 
-    app.celery_app = create_celery()
-    from project.users import users_router  # new
+    # do this before loading routes              # new
+    app.celery_app = create_celery()  # new
 
-    app.include_router(users_router)  # new
+    from project.users import users_router
+
+    app.include_router(users_router)
 
     @app.get("/")
     async def root():
--- project/config.py
+++ project/config.py
@@ -13,7 +13,7 @@ class BaseConfig:
 
     CELERY_BROKER_URL: str = os.environ.get(
         "CELERY_BROKER_URL", "redis://127.0.0.1:6379/0"
-    )
+    )  # NEW
     CELERY_RESULT_BACKEND: str = os.environ.get(
         "CELERY_RESULT_BACKEND", "redis://127.0.0.1:6379/0"
     )
